075a2994532e9c4b6c6f8a6e6c1b28641ce84ff2
Lena <elena.borisenko1991@gmail.com>
2019-02-21 15:35:34 +0000

    add currency list and comission list, make it work correctly

--- src/controller.js
+++ src/controller.js
@@ -1,14 +1,25 @@
 (function() {
   window.app.controller('myCtrl', function($scope) {
-    $scope.currencyFromAmmount = 0;
+    $scope.currencyFromAmmount = '';
     $scope.currencyToAmmount = '';
-    $scope.compute = function() {
-      console.log($scope.currencyFromAmmount * $scope.currencyToAmmount);
+    // $scope.compute = function() {
+    //   console.log($scope.currencyFromAmmount * $scope.currencyToAmmount);
+    // };
+    $scope.currencyList = ['USD', 'EUR', 'UAH', 'GBR', 'RUB'];
+    $scope.persentagesList = ['2%', '5%', '10%'];
+    $scope.baseCurrency = 'Base Currency';
+    $scope.targetCurrency = 'Target Currency';
+    $scope.comission = 'Comission';
+
+    $scope.changeTargetCurrency = function($event, item) {
+      if ($event.currentTarget.parentNode.previousElementSibling.innerHTML === $scope.baseCurrency) {
+        $scope.baseCurrency = item;
+      } else {
+        $scope.targetCurrency = item;
+      }
     };
-    $scope.currencyList = ['USD', 'EUR', 'UAH'];
-    $scope.currency = 'Currency';
-    $scope.changeTargetCurrency = function(item) {
-      $scope.currency = item;
+    $scope.changePersantage = function(elem) {
+      $scope.comission = elem;
     };
   });
 }());
