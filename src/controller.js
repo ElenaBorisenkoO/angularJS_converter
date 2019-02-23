3c7b861b48655cd25136e8b775467161ed07ddb2
Lena <elena.borisenko1991@gmail.com>
2019-02-23 17:03:52 +0000

    add calculation currency amount after exchange

--- src/controller.js
+++ src/controller.js
@@ -1,28 +1,51 @@
 (function() {
   window.app.controller('myCtrl', ['$scope', 'rateService', function($scope, rateService) {
-    $scope.exchangeRatesData = {};
+    // $scope.exchangeRatesData = {};
     rateService.getRates().then(data => {
-      console.log(data);
-      $scope.exchangeRatesData = data;
+      console.log(data[0].buy);
+      console.log(data[0].sale);
+      // console.log(data);
+
+      $scope.calculateAmount = function() {
+        data.forEach(elem => {
+          if (elem.ccy === $scope.baseCurrency) {
+            $scope.indicator === 'sale' ? $scope.currencyToAmmount = $scope.currencyFromAmmount * elem.sale :
+              $scope.currencyToAmmount = $scope.currencyFromAmmount * elem.buy;
+          }
+        });
+      };
+
+      // $scope.exchangeRatesData = data;
     })
       .catch(err => {
         console.log(err);
       });
+    $scope.indicator = '';
+    $scope.test = function($event) {
+      if ($event.currentTarget.firstElementChild.classList.contains('off')) {
+        $scope.indicator = 'sale';
+        console.log($scope.indicator);
+      } else {
+        $scope.indicator = 'buy';
+        console.log($scope.indicator);
+      }
+    };
+
 
     $scope.currencyFromAmmount = '';
-    $scope.currencyToAmmount = 10;
-    $scope.currencyList = ['USD', 'EUR', 'UAH', 'GBP', 'RUB'];
+    $scope.currencyToAmmount = '';
+    $scope.currencyList = ['USD', 'EUR', 'RUR', 'BTC', 'UAH'];
     $scope.persentagesList = ['2%', '5%', '10%']; // {label: 2%, data: 0.02}
     $scope.baseCurrency = 'Base Currency';
     $scope.targetCurrency = 'Target Currency';
     $scope.comission = 'Comission 0%';
 
-    $scope.inputCurrency = 0;
-    $scope.exchangeRate = 0;
-    $scope.outputTemp = 0;
-    $scope.fee = 0;
-    $scope.feeDirection = 1;
-    $scope.outputCurrency = 0;
+    // $scope.inputCurrency = 0;
+    // $scope.exchangeRate = 0;
+    // $scope.outputTemp = 0;
+    // $scope.fee = 0;
+    // $scope.feeDirection = 1;s
+    // $scope.outputCurrency = 0;
 
 
     $scope.changeTargetCurrency = function($event, item) {
@@ -43,9 +66,9 @@
       $scope.currencyToAmmount += elem.replace(/\D/g, '') / 100;
     };
 
-    $scope.applyToggleDirection = function(direction) {
-      $scope.feeDirection = direction * -1;
-      console.log($scope.feeDirection);
-    };
+    // $scope.applyToggleDirection = function(direction) {
+    //   $scope.feeDirection = direction * -1;
+    //   console.log($scope.feeDirection);
+    // };
   }]);
 }());
--- src/service.js
+++ src/service.js
@@ -1,11 +1,10 @@
 (function() {
   window.app.service('rateService', ['$http', function($http) {
-    // https://api.privatbank.ua/p24api/exchange_rates?json&date=20.02.2019
     console.log('requested API data');
     this.getRates = function() {
       return $http({
         method: 'GET',
-        url: 'https://www.cbr-xml-daily.ru/daily_json.js'
+        url: 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5'
       }).then(({ data }) => data);
     };
   }]);
