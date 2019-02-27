f628e4f469c1a17158208f3b66001cd7d9871ef0
Lena <elena.borisenko1991@gmail.com>
2019-02-27 13:13:44 +0000

    add provider insted servise

--- src/app.js
+++ src/app.js
@@ -1,22 +1,24 @@
+/* global angular*/
 (function() {
   window.app = angular.module('myApp', []);
-  // window.app.config(['rateServiceProvider', function(rateServiceProvider) {
-  //   console.log(rateServiceProvider);
-  //   rateServiceProvider.setAPI('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5');
-  // }
-  // ]);
-  // window.app.run(function($window, $rootScope) {
-  //   $rootScope.online = navigator.onLine;
-  //   $window.addEventListener('offline', function() {
-  //     $rootScope.$apply(function() {
-  //       $rootScope.online = false;
-  //     });
-  //   }, false);
+  window.app.config(['rateServiceProvider', function(rateServiceProvider) {
+    rateServiceProvider.setAPI('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5');
+  }
+  ]);
+  window.app.run(function($window, $rootScope) {
+    $rootScope.online = navigator.onLine;
+    // console.log($rootScope.online);
+    $window.addEventListener('offline', function() {
+      $rootScope.$apply(function() {
+        $rootScope.online = false;
+      });
+    }, false);
 
-  //   $window.addEventListener('online', function() {
-  //     $rootScope.$apply(function() {
-  //       $rootScope.online = true;
-  //     });
-  //   }, false);
-  // });
+    $window.addEventListener('online', function() {
+      $rootScope.$apply(function() {
+        $rootScope.online = true;
+      });
+    }, false);
+    console.log($rootScope.online);
+  });
 }());
--- src/service.js
+++ src/service.js
@@ -1,38 +1,47 @@
-/* global myApp, angular*/
 (function() {
-  window.app.service('rateService', ['$http', function($http) {
-    this.list = [];
-    this.getRates = function() {
-      if (this.list.length === 0) {
-        $http.get('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5')
-          .then(({ data }) => {
-            data.forEach(item => this.list.push(item));
-          });
-      }
-      return this.list;
-    };
-    this.computeRate = function(base, target) {
-      return base / target;
-    };
-    this.computeFee = function(data, fee, buy) {
-      const sign = buy ? 1 : -1;
+  window.app.provider('rateService', function() {
+    let API = '';
 
-      return data * fee * sign / 100;
+    return {
+      setAPI: apiUrl => (API = apiUrl),
+
+      list: [],
+
+      $get: ['$http', function($http) {
+        return {
+          getRates: () => {
+            $http.get(API)
+              .then(({ data }) => {
+                data.forEach(item => this.list.push(item));
+              });
+
+            return this.list;
+          },
+          computeRate(base, target) {
+            return base / target;
+          },
+          computeFee(data, fee, buy) {
+            const sign = buy ? 1 : -1;
+
+            return data * fee * sign / 100;
+          },
+
+          calculateAmount(currencyFrom, currencyTo, amount, buy) {
+            if (!currencyFrom.$$hashKey || !currencyTo.$$hashKey) {
+              return 0;
+            }
+
+            const rate = buy ? currencyFrom.buy / currencyTo.buy : currencyFrom.sale / currencyTo.sale;
+
+            return {
+              rate,
+              'count': amount * rate
+            };
+          }
+        };
+      }]
     };
-
-    this.calculateAmount = function(currencyFrom, currencyTo, amount, buy) {
-      if (!currencyFrom.$$hashKey || !currencyTo.$$hashKey) {
-        return 0;
-      }
-
-      const rate = buy ? currencyFrom.buy / currencyTo.buy : currencyFrom.sale / currencyTo.sale;
-
-      return {
-        rate,
-        'count': amount * rate
-      };
-    };
-  }]);
+  });
   window.app.constant('converterConstants', {
     'persentagesList': [0, 2, 5, 10]
   });
