de9cf03a9cc0b999ed32267d586d9aec974736d4
Lena <elena.borisenko1991@gmail.com>
2019-02-27 14:55:38 +0000

    add directives file

--- src/app.js
+++ src/app.js
@@ -5,11 +5,6 @@
     rateServiceProvider.setAPI('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5');
   }
   ]);
-  window.app.directive('checkInternet', function() {
-    return {
-      restrict: 'A'
-    };
-  });
 
   window.app.run(function($rootScope) {
     $rootScope.online = navigator.onLine;
