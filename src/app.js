24e17f7ce0e93e012ee1e1f4187601ca14c0ec47
Lena <elena.borisenko1991@gmail.com>
2019-02-27 14:42:48 +0000

    add interner checker

--- src/app.js
+++ src/app.js
@@ -5,20 +5,13 @@
     rateServiceProvider.setAPI('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5');
   }
   ]);
-  window.app.run(function($window, $rootScope) {
-    $rootScope.online = navigator.onLine;
-    // console.log($rootScope.online);
-    $window.addEventListener('offline', function() {
-      $rootScope.$apply(function() {
-        $rootScope.online = false;
-      });
-    }, false);
+  window.app.directive('checkInternet', function() {
+    return {
+      restrict: 'A'
+    };
+  });
 
-    $window.addEventListener('online', function() {
-      $rootScope.$apply(function() {
-        $rootScope.online = true;
-      });
-    }, false);
-    console.log($rootScope.online);
+  window.app.run(function($rootScope) {
+    $rootScope.online = navigator.onLine;
   });
 }());
